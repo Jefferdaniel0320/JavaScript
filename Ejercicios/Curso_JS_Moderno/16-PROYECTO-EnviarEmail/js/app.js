fde25b1b129dc18bdca7f085d24051fb86580462
JefersonRomero <j.daniel0320@gmail.com>
2023-10-31 18:36:12 -0500

    Envio de mail finalizado

--- Ejercicios/Curso_JS_Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Ejercicios/Curso_JS_Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -9,8 +9,10 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
-    
     const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner');
 
     // Asignar eventeos
     /* 
@@ -34,20 +36,51 @@ document.addEventListener('DOMContentLoaded', function(){
    // Forma de reducir el anterior script:
 
    // Asignar eventeos
-   inputEmail.addEventListener('blur', validar);   
-   inputAsunto.addEventListener('blur', validar);   
-   inputMensaje.addEventListener('blur', validar);
+   inputEmail.addEventListener('input', validar);   
+   inputAsunto.addEventListener('input', validar);   
+   inputMensaje.addEventListener('input', validar);
+   formulario.addEventListener('submit', enviarEmail);
+   
+   btnReset.addEventListener('click', function(e) {
+    e.preventDefault();
+    resetFormulario();
+   })
+
+   function enviarEmail(e) {
+    e.preventDefault();
+    spinner.classList.add('flex');
+    spinner.classList.remove('hidden');
+    
+    setTimeout(() => {
+        spinner.classList.remove('flex');
+        spinner.classList.add('hidden');
+        resetFormulario();
+        // Crear una alerta
+        const alertaExito = document.createElement('P');
+        alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 
+        'rounded-lg', 'mt-10', 'font-bold', 'text-sm', 'uppercase');
+        alertaExito.textContent = 'Mensaje enviado Correctamente';
+        formulario.appendChild(alertaExito);
+        setTimeout(() => {
+            alertaExito.remove();
+        }, 3000);
+    }, 3000);
+   }
    
    function validar(e) {
     if(e.target.value.trim() === '') {
         // trim() valida si hay espacios en el campo
         // ${e.target.id}, me trae el id, el cual corresponde a: mail, asunto, mensaje, esto para personalizar
         mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+        email[e.target.name] = '';
+        comprobarEmail();
         return;
     }
-
+    
     if(e.target.id === 'email' && !validarEmail(e.target.value)) {
         mostrarAlerta('El email no es valido', e.target.parentElement);
+        email[e.target.name] = '';
+        comprobarEmail();
         return;
     }
 
@@ -93,7 +126,22 @@ document.addEventListener('DOMContentLoaded', function(){
 
     function comprobarEmail() {
         // Captura todos los objetos del arreglo y valida si algunno contiene un string vacio
-        console.log(Object.values(email).includes(''));
+        if(Object.values(email).includes('')) {
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.disabled = true;
+            return;
+        }
+        btnSubmit.classList.remove('opacity-50');
+        btnSubmit.disabled = false;
+    }
+
+    function resetFormulario() {
+        // Reiniciar el objeto
+        email.email = '';
+        email.asunto = '';
+        email.mensaje = '';
+        formulario.reset();
+        comprobarEmail();
     }
     
 });
