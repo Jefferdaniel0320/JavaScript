68fb89d13cd51c11137f5dbc8ebdca45abff3f4b
JefersonRomero <j.daniel0320@gmail.com>
2023-12-06 22:27:04 -0500

    Correcciones ejercicios

--- Ejercicios/Curso_JS_Moderno/13-DOM/js/01-scripts.js
+++ Ejercicios/Curso_JS_Moderno/13-DOM/js/01-scripts.js
@@ -2,14 +2,15 @@ let elemento;
 
 elemento = document; // aca se selecciona todo el html
 elemento = document.all; // aca se selecciona todo el html
-elemento = document.head; // aca se selecciona los diferentes metodos
+elemento = document.head; // aca se selecciona los diferentes metodos, es la parte superior
+elemento = document.body; // aca se selecciona los diferentes metodos, dentro etiqueta body
 elemento = document.domain; // aca se visualiza el dominio atual
 elemento = document.forms; // aca se visualiza el total de formularios
 elemento = document.forms[0]; 
 elemento = document.forms[0].id; 
 elemento = document.forms[0].method; 
 elemento = document.forms[0].classList; 
-elemento = document.forms[0].action; 
+elemento = document.forms[0].action;
 
 elemento = document.links; 
 elemento = document.links[4]; 
