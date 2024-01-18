b2570297edd27fa4040a833db10450a56e1f2a9a
JefersonRomero <j.daniel0320@gmail.com>
2024-01-17 23:54:45 -0500

    Se actualizo los eventos

--- Ejercicios/Curso_JS_Moderno/13-DOM/js/11-scripts.js
+++ Ejercicios/Curso_JS_Moderno/13-DOM/js/11-scripts.js
@@ -1,17 +1,18 @@
+
 const btnFlotante = document.querySelector('.btn-flotante');
 const footer = document.querySelector('.footer');
 
-btnFlotante.addEventListener('click', mostrarOcultarFooer);
+btnFlotante.addEventListener('click', mostrarOcultarFooter);
 
-function mostrarOcultarFooer() {
-    if (footer.classList.contains('activo')) {
-        footer.classList.remove('activo');
-        this.classList.remove('activo');
-        // btnFlotante.classList.remove('activo'); // este es igual al anterior, porque se hace relacion btnFlotante
-        this.textContent = 'Idioma y Moneda';
+function mostrarOcultarFooter() {
+    if(footer.classList.contains('activo')) {
+        footer.classList.remove('activo')
+        // btnFlotante.classList.remove('activo')
+        this.classList.remove('activo') // Es lo mismo que el de arriba, a que se ejecuta en la linea 4 o en el btnFlotante
+        this.textContent = 'Idioma y Moneda'
     } else {
-        footer.classList.add('activo');        
-        btnFlotante.classList.add('activo');
-        this.textContent = 'X Cerrar';
+        footer.classList.add('activo');
+        this.classList.add('activo')
+        this.textContent = 'X Cerrar'
     }
 }
